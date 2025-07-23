0d22002f313a088831cb5b10997676d63f67ecae
Tatakung <tasawa.012@gmail.com>
2025-07-23 14:40:14 +0700

    enjoyjube

--- src/App.jsx
+++ src/App.jsx
@@ -168,6 +168,12 @@ function App() {
                         data-bs-slide-to="5"
                         aria-label="Slide 6"
                       ></button>
+                      <button
+                        type="button"
+                        data-bs-target="#projectCarousel1"
+                        data-bs-slide-to="6"
+                        aria-label="Slide 7"
+                      ></button>
                     </div>
                     <div class="carousel-inner">
                       <div class="carousel-item active">
@@ -436,6 +442,12 @@ function App() {
                         data-bs-target="#projectCarousel3"
                         data-bs-slide-to="4"
                         aria-label="Slide 5"
+                      ></button>
+                      <button
+                        type="button"
+                        data-bs-target="#projectCarousel3"
+                        data-bs-slide-to="5"
+                        aria-label="Slide 6"
                       ></button>
                     </div>
                     <div class="carousel-inner">
